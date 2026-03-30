8daccaf11fc6b9662da62c79df061482cc0a7124
MNPPI223 <mnppi223@gmail.com>
2026-03-30 17:50:51 +0900

    Service 영역을 위해 또한 저희 추가 파라미터가 들어오는 유지관점 차원으로 DAO 부분을 수정했습니다.

--- src/main/java/com/wanted/crud/dao/EmployeeRegistDAO.java
+++ src/main/java/com/wanted/crud/dao/EmployeeRegistDAO.java
@@ -1,15 +1,22 @@
 package com.wanted.crud.dao;
 
 
+import com.wanted.crud.dto.EmployeeRegistDTO;
 import com.wanted.crud.global.JDBCTemplate;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class EmployeeRegistDAO {
 
-    public void employeeRegist(String empId, String empName, String email,
-                               String phone, String deptCode, String jobCode,
-                               int salary, String hireDate, String entYn) {
+    /* comment.
+        기존의 EmpId ~ EntYn 9개의 개별 파라미터를 DTO 1개로 변경
+        1. 가독성 : DTO 1개가 훨씬 읽기 쉬움
+        2. 유지보수 : 필드 추가, 삭제 관점에서 DTO만 수정하면 문제 없음
+        3. 일관성 : Service 에서 DAO 로 데이터 전달 시 DTO 객체 그대로 넘기면 됨
+        4. 확장성 : 나중에 파라미터가 늘어나도 메서드 시기니처 변경하지 않아도 됨
+     */
+
+    public void employeeRegist(EmployeeRegistDTO dto) {
         // SQL 쿼리문 (사원번호, 사원명, 이메일, 전화번호, 부서코드, 직급코드, 급여, 입사일, 퇴직여부)
         String sql = "INSERT INTO EMPLOYEE (EMP_ID, EMP_NAME, EMAIL, PHONE, DEPT_CODE, JOB_CODE, SALARY, HIRE_DATE, ENT_YN)" +
         " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -21,15 +28,15 @@ public class EmployeeRegistDAO {
         try {
             conn = JDBCTemplate.getConnection();
             pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, empId);
-            pstmt.setString(2, empName);
-            pstmt.setString(3, email);
-            pstmt.setString(4, phone);
-            pstmt.setString(5, deptCode);
-            pstmt.setString(6, jobCode);
-            pstmt.setInt(7, salary);
-            pstmt.setString(8, hireDate);
-            pstmt.setString(9, entYn);
+            pstmt.setString(1, dto.getEmpId());
+            pstmt.setString(2, dto.getEmpName());
+            pstmt.setString(3, dto.getEmail());
+            pstmt.setString(4, dto.getPhone());
+            pstmt.setString(5, dto.getDeptCode());
+            pstmt.setString(6, dto.getJobCode());
+            pstmt.setInt(7, dto.getSalary());
+            pstmt.setString(8, dto.getHireDate());
+            pstmt.setString(9, dto.getEntYn());
 
             // 쿼리를 실행하는 코드
             pstmt.executeUpdate();
--- src/main/java/com/wanted/crud/service/EmployeeRegistService.java
+++ src/main/java/com/wanted/crud/service/EmployeeRegistService.java
@@ -1,4 +1,7 @@
 package com.wanted.crud.service;
 
 public class EmployeeRegistService {
+
+
+
 }

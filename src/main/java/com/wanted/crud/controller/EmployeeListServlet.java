7db4ce2fa96509fb87aaa824649b0b3db4ee7bd5
GANGHYEON02 <masan8259@gmail.com>
2026-03-31 00:38:24 +0900

    전체 사원 조회 완료

--- src/main/java/com/wanted/crud/controller/EmployeeListServlet.java
+++ src/main/java/com/wanted/crud/controller/EmployeeListServlet.java
@@ -1,4 +1,29 @@
 package com.wanted.crud.controller;
 
-public class EmployeeListServlet {
+import com.wanted.crud.dto.EmployeeListDTO;
+import com.wanted.crud.service.EmployeeListService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/employees")
+public class EmployeeListServlet extends HttpServlet {
+
+    private final EmployeeListService employeeListService = new EmployeeListService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<EmployeeListDTO> emplist = employeeListService.findAllEmp();
+        req.setAttribute("emplist", emplist);
+
+        RequestDispatcher rd = req.getRequestDispatcher("/view/employee/list.jsp");
+        rd.forward(req, resp);
+    }
 }
--- src/main/java/com/wanted/crud/service/EmployeeListService.java
+++ src/main/java/com/wanted/crud/service/EmployeeListService.java
@@ -1,4 +1,22 @@
 package com.wanted.crud.service;
 
+import com.wanted.crud.dao.EmployeeListDAO;
+import com.wanted.crud.dto.EmployeeListDTO;
+import com.wanted.crud.global.JDBCTemplate;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class EmployeeListService {
+
+    private final EmployeeListDAO employeeListDAO = new EmployeeListDAO();
+
+    public List<EmployeeListDTO> findAllEmp() {
+
+        Connection con = JDBCTemplate.getConnection();
+        List<EmployeeListDTO> resultList = employeeListDAO.empSelect(con);
+        JDBCTemplate.close(con);
+
+        return resultList;
+    }
 }
